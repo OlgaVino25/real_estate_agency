a81e65eeef4f67cd5741700526ad5b41c07baaba
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-09-03 13:49:22 +0300

    formatted pep8

--- property/admin.py
+++ property/admin.py
@@ -2,18 +2,28 @@ from django.contrib import admin
 
 from .models import Flat, Complaint
 
+
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town','address', 'owner')
-    readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
-    list_editable = ('new_building',)
-    list_filter = ('new_building', 'rooms_number', 'has_balcony')
-    raw_id_fields = ('likes',)
+    search_fields = ("town", "address", "owner")
+    readonly_fields = ("created_at",)
+    list_display = (
+        "address",
+        "price",
+        "new_building",
+        "construction_year",
+        "town",
+        "owners_phonenumber",
+        "owner_pure_phone",
+    )
+    list_editable = ("new_building",)
+    list_filter = ("new_building", "rooms_number", "has_balcony")
+    raw_id_fields = ("likes",)
 
 
 class ComplaintAdmin(admin.ModelAdmin):
-    raw_id_fields = ('flat',)
-    list_display = ('user', 'flat', 'text')
+    raw_id_fields = ("flat",)
+    list_display = ("user", "flat", "text")
+
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
